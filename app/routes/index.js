fcfc9aebc6734aee9f63cc4ae584e6ef197cedaa
Ruchi Jain <ruchi@MacBook-Pro-2.home>
2018-04-15 15:56:57 -0400

    Fix

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,18 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  var data = req.app.get('appData');
+  var dataFile = req.app.get('appData');//points to whole json file
   var pagePhotos = [];
-  var pageSpeakers = data.speakers;
-
-  data.speakers.forEach(function(item) {
-    pagePhotos = pagePhotos.concat(item.artwork);
+  var pageHighlights = dataFile.data.highlights;//present in json
+//since speaker is array so we iterate through it
+  dataFile.data.highlights.forEach(function(item) {
+    pagePhotos = pagePhotos.concat(item.image);
   });
 
   res.render('index', {
     pageTitle: 'Home',
     artwork: pagePhotos,
-    speakers: pageSpeakers,
+    highlights: pageHighlights,
     pageID: 'home'
   });
 
